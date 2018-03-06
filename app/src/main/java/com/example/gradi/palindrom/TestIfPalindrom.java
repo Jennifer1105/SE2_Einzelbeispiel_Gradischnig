fc25fcbca8274faa1fef786908570ed42dbb29bd
Jennifer1105 <gradischnig.jennifer@gmx.at>
2018-03-06 10:44:24 +0100

    added files

---
+++ app/src/main/java/com/example/gradi/palindrom/TestIfPalindrom.java
@@ -0,0 +1,13 @@
+package com.example.gradi.palindrom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestIfPalindrom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_if_palindrom);
+    }
+}
